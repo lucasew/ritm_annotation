cd4ac41ab71f1ef1cad7c8763eaf5c4aa31cd237
lucasew <lucasew@users.noreply.github.com>
2023-07-27 13:31:52 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="lucasew"
+original_name="ritm_annotation"
+original_urlname="ritm_annotation"
+original_description="Awesome ritm_annotation created by lucasew"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv ritm_annotation $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ ritm_annotation/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for ritm_annotation."""
+
+from ritm_annotation.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
