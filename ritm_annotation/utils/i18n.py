718512e35d2707c075e98e33cac150d0ce361153
lucasew <lucas59356@gmail.com>
2023-08-21 17:18:55 -0300

    locale: simplificação da lógica de carregamento de tradução
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- ritm_annotation/utils/i18n.py
+++ ritm_annotation/utils/i18n.py
@@ -5,12 +5,11 @@ logger = logging.getLogger(__name__)
 
 locale_dir = Path(__file__).parent.parent / "i18n"
 
-translations = gettext.translation(
+gettext.install(
     'ritm_annotation',
     localedir=str(locale_dir),
-    fallback=True,
-    # languages=['en'],
 )
-translations.install()
+
+gettext.gettext = _
 
 logger.debug(_('Loading locale data from "{locale_folder}"').format(locale_folder=locale_dir))
