768f75b6c19a646a55ca498f1173d1228b4ebb96
lucasew <lucas59356@gmail.com>
2024-02-21 17:14:59 -0300

    cli: show default value for flags in all subcommands
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- ritm_annotation/cli/__init__.py
+++ ritm_annotation/cli/__init__.py
@@ -21,7 +21,11 @@ logger = logging.getLogger(__name__)
 
 
 def add_subcommand(subparsers, name: str, submodule):
-    subparser = subparsers.add_parser(name, help=submodule.COMMAND_DESCRIPTION)
+    subparser = subparsers.add_parser(
+        name,
+        help=submodule.COMMAND_DESCRIPTION,
+        formatter_class=ArgumentDefaultsHelpFormatter
+    )
     common_flags(subparser)
     handler = submodule.command(subparser)
     subparser.set_defaults(fn=handler)
@@ -62,7 +66,8 @@ def main():  # pragma: no cover
     """
     logging.basicConfig()
     parser = ArgumentParser(
-        prog="ritm_annotation", formatter_class=ArgumentDefaultsHelpFormatter
+        prog="ritm_annotation",
+        formatter_class=ArgumentDefaultsHelpFormatter
     )
     common_flags(parser)
     subparsers = parser.add_subparsers()
