141ca30d550681ebd825cd3011d106a93b57e850
lucasew <lucas59356@gmail.com>
2025-07-26 13:02:39 -0300

    actions: locale update, nix binary caches
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,17 @@
 {
   description = "Tool to do dataset annotation for semantic segmentation datsets";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://cuda-maintainers.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
