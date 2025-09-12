a87e89f08d6984226e592d0e7442a2d522a94c69
lucasew <lucas59356@gmail.com>
2025-09-12 16:58:23 -0300

    prebuild wheels setup
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import numpy as np
+from pathlib import Path
+
+extensions = [
+    Extension(
+        "ritm_annotation/utils/cython/_get_dist_maps",
+        sources=["ritm_annotation/utils/cython/_get_dist_maps.pyx"],
+        include_dirs=[np.get_include()]
+    )
+]
+
+setup(
+    name="ritm_annotation",
+    version=Path("./ritm_annotation/VERSION").read_text().strip(),
+    ext_modules = cythonize(extensions),
+    py_modules=["ritm_annotation"]
+)
