040ecf03f76ba0f6c40d7111f56157519cd3d32d
lucasew <lucas59356@gmail.com>
2025-09-21 21:50:00 -0300

    dagger: codegen fixes
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- .dagger/src/ritm_annotation/main.py
+++ .dagger/src/ritm_annotation/main.py
@@ -1,9 +1,45 @@
 import dagger
-from dagger import dag, function, object_type
-
+from dagger import dag, function, object_type, DefaultPath
+from typing import Annotated
 
 @object_type
 class RitmAnnotation:
+    @function
+    def gettext_container(self) -> dagger.Container:
+        return dag.container().from_("alpine:latest").with_exec(["apk", "add", "gettext", "bash"])
+
+    @function
+    async def codegen_fixes(self, source: dagger.Directory) -> dagger.Directory:
+        output = source
+        output = await self.update_locales(output)
+        return output
+
+    @function
+    async def update_locales(self, source: dagger.Directory) -> dagger.Directory:
+        return (
+            await self.gettext_container()
+                .with_mounted_directory("/src", source)
+                .with_workdir("/src")
+                .with_exec(["./update_locales"])
+                .directory("/src")
+        )
+
+    @function
+    async def test_update_date(self, source: dagger.Directory):
+        return (
+            await dag.container()
+                .from_("alpine:latest")
+                .with_mounted_directory("/src", source)
+                .with_exec(["/bin/sh", "-c", "date > /src/date.txt"])
+                .with_exec(["ls", "/src"])
+                .directory("/src")
+                # .export(".") # you need to run export in the cli
+        )
+
+    # @function
+    # def update_locale(self, locale: str):
+        
+    
     @function
     def container_echo(self, string_arg: str) -> dagger.Container:
         """Returns a container that echoes whatever string argument is provided"""
